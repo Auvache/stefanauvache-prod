e90bd1b1b6bed7eb545fe05f84d7288035852a85
Stefan Bradley <sbradley@taxhawk.com>
2025-02-19 12:47:38 -0800

    testing nuxt ssg

---
+++ _nuxt/BQLGReUN.js
@@ -0,0 +1,1 @@
+import{_ as n,c as t,o as a,b as c,k as r,l as s}from"./w8H2frjm.js";const _={};function l(e,f){const o=s;return a(),t("div",null,[c(o),r(e.$slots,"default")])}const m=n(_,[["render",l]]);export{m as default};
