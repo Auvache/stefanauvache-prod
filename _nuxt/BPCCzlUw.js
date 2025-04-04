0f425658be0e7cb5429982a36fb56b7ffaa381f6
sbradley <sbradley@taxhawk.com>
2025-04-04 14:07:09 -0700

    prod push new articles

--- _nuxt/BPCCzlUw.js
+++
@@ -1,1 +0,0 @@
-import{g as s,c as n,a as t,o as c}from"./CcN-84rg.js";const a={},o={class:"section-padding"};function i(l,e){return c(),n("section",o,e[0]||(e[0]=[t("div",{class:"container thinnest-content"},[t("div",null,[t("h1",{class:"mb-3"},"title"),t("p",{class:"tagline fst-italic blue mb-3"},"month 1, 2025")])],-1)]))}const d=s(a,[["render",i]]);export{d as default};
